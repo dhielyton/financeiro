63990360e9f42616aa03e8a08cbbc5ab856d0ffb
dhielyton <dhielyton@gmail.com>
2025-05-22 20:56:29 -0300

    feature: inclusão da classe de conta no dominio, com as regras e validações da inclusão de uma conta master. test: implemtação dos cenários de testes, validando quando uma conta for definida com pai.

---
+++ Controle.Financeiro/Controle.Financeiro.Domain/PlanoContas/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle.Financeiro.Domain.PlanoContas
+{
+    public class Conta
+    {
+        public Conta(string codigo, string descricao, TipoConta tipo, bool aceitaLancamento)
+        {
+            Id = Guid.NewGuid().ToString();
+            Descricao = descricao;
+            Codigo = codigo;
+            Tipo = tipo;
+            AceitaLancamento = aceitaLancamento;
+        }
+
+        public string Id { get; set; }
+        public Conta ContaMaster { get; private set; }
+        public string ContaMasterId { get; set; }
+        public string Descricao { get; set; }
+        public string Codigo { get; set; }
+        public TipoConta Tipo { get; set; }
+        public bool AceitaLancamento { get; set; }
+
+        public void AddContarMaster(Conta conta)
+        {
+            if (conta.Tipo != Tipo)
+                throw new InvalidOperationContasTiposIncompativeisException();
+            if (conta.AceitaLancamento)
+                throw new InvalidOperationContaMasterAceitaLancamentoIsTrueException();
+
+            ContaMaster = conta;
+            ContaMasterId = conta.Id;
+        }
+
+
+
+    }
+}
--- Controle.Financeiro/Controle.Financiero.Test/PlanoContas/PlanoContaTest.cs
+++ Controle.Financeiro/Controle.Financiero.Test/PlanoContas/PlanoContaTest.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using Controle.Financeiro.Domain.PlanoContas;
+using Controle.Financeiro.Domain.PlanoContas.Exceptions;
+using FluentAssertions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,53 +16,46 @@ namespace Controle.Financiero.Test.PlanoContas
         {
             var conta = new Conta("1", "Receitas", Tipo.Receita, false);
             conta.Descricao.Should().NotBeNullOrEmpty();
-            conta.Tipo.Should().Be(Tipo.Receita);
+            conta.Tipo.Should().Be(TipoConta.Receita);
             conta.AceitaLancamento.Should().BeFalse();
         }
 
         [Fact]
         public void CadastrarContaComPaiComSucesso()
         {
-            var conta = new Conta("1", "Receitas", Tipo.Receita, false);
-            var subconta = new Conta("1.1","Taxa Condominial", Tipo.Receita, true);
+            var conta = new Conta("1", "Receitas", TipoConta.Receita, false);
+            var subconta = new Conta("1.1","Taxa Condominial", TipoConta.Receita, true);
             subconta.AddContarMaster(conta);
             subconta.Descricao.Should().NotBeNullOrEmpty();
-            subconta.Tipo.Should().Be(Tipo.Receita);
+            subconta.Tipo.Should().Be(TipoConta.Receita);
             subconta.AceitaLancamento.Should().BeTrue();
             subconta.ContaMaster.Should().NotBeNull();
         }
-    }
 
-    public class Conta
-    {
-        public Conta(string codigo, string descricao, Tipo tipo, bool aceitaLancamento)
+        [Fact]
+        public void AgruparContasComTiposDiferentes()
         {
-
-            Descricao = descricao;
-            Codigo = codigo;
-            Tipo = tipo;
-            AceitaLancamento = aceitaLancamento;
+            var conta = new Conta("1", "Receitas", TipoConta.Receita, false);
+            var subconta = new Conta("2.1", "Salario", TipoConta.Despesa, true);
+            Action act = () => subconta.AddContarMaster(conta);
+            act.Should().Throw<InvalidOperationContasTiposIncompativeisException>();
         }
 
-        public Guid Id { get; set; }
-        public Conta ContaMaster { get; private set; }
-        public string Descricao { get; set; }
-        public string Codigo { get; set; }
-        public Tipo Tipo { get; set; }
-        public bool AceitaLancamento { get; set; }
-
-        public void AddContarMaster(Conta conta)
+        [Fact]
+        public void AdicionarContaFilhaUmaContaQueAceitaLancamentos()
         {
-            ContaMaster = conta;
+            var conta = new Conta("2.1.4", "Ferias", TipoConta.Despesa, true);
+            var subconta = new Conta("2.1.2", "Salario", TipoConta.Despesa, true);
+            Action act = () => subconta.AddContarMaster(conta);
+            act.Should().Throw<InvalidOperationContaMasterAceitaLancamentoIsTrueException>();
         }
-
-
-
     }
 
-    public enum Tipo
-    {
-        Receita,
-        Despesa
-    }
+    
+
+    
+
+    
+
+    
 }
